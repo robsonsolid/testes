384322049bce7f0d1f843d2cc057b20590d217bf
Robson Castaldi <castaldirobson@outlook.com>
2020-06-29 15:56:25 -0300

    Alteração Robson

--- Agenda/Agenda.aspx.cs
+++ Agenda/Agenda.aspx.cs
@@ -13,7 +13,7 @@ public partial class _Default : System.Web.UI.Page
 
     protected void Page_Load(object sender, EventArgs e)
     {
-        hdnLiberacao.Value = "0";
+        hdnLiberacao.Value = "2";
     }
 
 
